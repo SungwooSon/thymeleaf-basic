f35b0c8a2b88f213ade89089468a609e31c44c7f
sungwoo <ssw890418@gmail.com>
2021-06-28 14:41:38 +0900

    initail commit

---
+++ src/main/java/hello/thymeleaf/ThymeleafBasicApplication.java
@@ -0,0 +1,14 @@
+package hello.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBasicApplication {
+
+	public static void main(String[] args) {
+		System.out.println("say hi");
+		SpringApplication.run(ThymeleafBasicApplication.class, args);
+	}
+
+}
